5fddb45164a618df8f5b3fccf73ab9841b9abb4d
Kim taegyu <yjh5331ang@naver.com>
2025-03-20 00:20:20 +0900

    data_structure_week1_hw

--- hanoi_tower.c
+++ hanoi_tower.c
@@ -6,18 +6,18 @@ void hanoi(int n, char from, char middle, char to)
 {
     if (n >= 1) {
         hanoi(n - 1, from, to, middle);
-        printf("%d�� ������ %c���� %c�� �̵�\n", n, from, to);
+        printf("%d번 원판을 %c에서 %c로 이동\n", n, from, to);
         num++;
         hanoi(n - 1, middle, from, to);
     }
 }
 int main() {
     int n;
-    printf("������ ������ �Է��Ͻÿ�: ");
+    printf("원판의 개수를 입력하시오: ");
     scanf("%d", &n);
 
     hanoi(n, 'A', 'B', 'C');
-    printf("�� �̵� Ƚ��: %d", num);
+    printf("총 이동 횟수: %d", num);
     
     return 0;
 }
